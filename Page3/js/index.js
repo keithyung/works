7fbcc39035ffa54bd716990ab7612778189f9215
yung keith <yang.z.cn@gmail.com>
2015-05-13 14:50:07 +0800

    修改

---
+++ Page3/js/index.js
@@ -0,0 +1,19 @@
+
+$(function() {
+
+	var script=document.createElement("script");
+	script.type="text/javascript";
+	if(script.readyState){//IE
+		script.onreadystatechange=function(){
+	        console.log('livereload.js leaded.');
+		};
+	}else{//Others
+		script.onload=function(){
+	        console.log('livereload.js leaded.');
+		};
+	}
+	script.src='/vendor/livereload.js?_'+new Date().getTime();
+	document.getElementsByTagName("head")[0].appendChild(script);
+
+});
+
