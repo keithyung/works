d8f2673e9e5bfcfe72e62e2186672a167f9fabba
yung keith <yang.z.cn@gmail.com>
2015-05-13 14:50:02 +0800

    修改

--- Page1/js/index.js
+++ Page1/js/index.js
@@ -1,2 +1,5 @@
 
-
+var script=document.createElement("script");
+script.type="text/javascript";
+script.src='/vendor/livereload.js?_'+new Date().getTime();
+document.getElementsByTagName("head")[0].appendChild(script);
--- Page2/js/index.js
+++ Page2/js/index.js
@@ -1,7 +1,10 @@
 
 $(function() {
 
-	
+	var script=document.createElement("script");
+	script.type="text/javascript";
+	script.src='/vendor/livereload.js?_'+new Date().getTime();
+	document.getElementsByTagName("head")[0].appendChild(script);
 
 });
 
